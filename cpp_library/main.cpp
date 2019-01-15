d982de609d998671ea7b12c7463699e7268a5d79
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2019-01-15 11:10:20 -0500

    Moved compiler over to python, about to build parser

--- cpp_library/main.cpp
+++ cpp_library/main.cpp
@@ -6,110 +6,132 @@
 #include "logic.cpp"
 using namespace std;
 
-class PrintFive : public Function {
-public:
-        template<typename __A__, typename __B__, typename __C__, typename __D__, typename __E__>
-        auto call(__A__ a, __B__ b, __C__ c, __D__ d, __E__ e) {
-                return Print().call(a, b, c, d, e);
-        }
-};
+// class PrintFive : public Function {
+// public:
+//         template<typename __A__, typename __B__, typename __C__, typename __D__, typename __E__>
+//         auto call(__A__ a, __B__ b, __C__ c, __D__ d, __E__ e) {
+//                 return Print().call(a, b, c, d, e);
+//         }
+// };
 
 
+// class Square : public Function {
+// public:
+//     template<typename __A__>
+//     auto call(__A__ a) {
+//         return Mul().call(a, a);
+//     }
+// };
+
+
+// class Y : public Function {
+// public:
+//     template<typename __A__, typename __B__>
+//     auto call(__A__ a, __B__ b) {
+//         auto result = a.call(b);
+//         while (true) {
+//             result = a.call(result);
+//         }
+//         return result;
+//     }
+// };
+
+
+// class Pipe : public Function {
+// public:
+//     template<typename __A__, typename __B__, typename __C__>
+//     auto call(__A__ a, __B__ b, __C__ c) {
+//         return a.call(b.call(c));
+//     }
+// };
+
+
+// class Main : public Function {
+// public:
+//     Function call() {
+//         // PrintFive()
+//         //     .call(
+//         //         String("Test"),
+//         //         String("ing"),
+//         //         String(" "),
+//         //         String("Hmm"),
+//         //         String("\n")
+//         //         );
+
+//         // Y().call(
+//         //     Print(), String("Test")
+//         //     );
+//         Pipe().call(Println(), Square(), Number(200));
+//         // Pipe().call(Print(), Square(), Number(100));
+
+//         Print().call(String("square: "), Square().call(Number(500)), String("\n"));
+
+//         Assert()
+//             .call(
+//                 If().call(Identity().call(False()), False(), True())
+//             );
+
+//         Debug().call(
+//             If().call(Identity().call(True()), String("Ok"), String("Not Ok"))
+//         );
+
+//         Debug().call(
+//             If().call(
+//                 Equals().call(String("1"), String("2")),
+//                     Number(1), Number(2.7646)
+//                 )
+//         );
+
+//         Debug().call(
+//             Equals()
+//                 .call(Println(), Println())
+//         );
+
+//         Debug().call(
+//             Add()
+//                 .call(Number(1), Number(2))
+//         );
+
+//         Debug().call(
+//             Identity().call(Div()
+//                 .call(Number(13), Number(2)))
+//         );
+
+//         Debug().call(
+//             False().call(
+//                 True().call(String("A"), String("B")),
+//                 True().call(String("C"), String("D"))
+//             )
+//         );
+
+//         return Function();
+//     }
+// };
 class Square : public Function {
 public:
-    template<typename __A__>
-    auto call(__A__ a) {
-        return Mul().call(a, a);
-    }
-};
-
-
-class Y : public Function {
-public:
-    template<typename __A__, typename __B__>
-    auto call(__A__ a, __B__ b) {
-        auto result = a.call(b);
-        while (true) {
-            result = a.call(result);
+        template<typename __A__>
+        auto call(__A__ a) {
+                return Mul().call(a, a);
         }
-        return result;
-    }
 };
-
-
-class Pipe : public Function {
+class Test : public Function {
 public:
-    template<typename __A__, typename __B__, typename __C__>
-    auto call(__A__ a, __B__ b, __C__ c) {
-        return a.call(b.call(c));
-    }
+        template<typename __A__>
+        auto call(__A__ a) {
+                Print().call(String("Test"));
+                Println().call(String("ing"));
+                return Println().call(a);
+        }
 };
-
-
 class Main : public Function {
 public:
-    Function call() {
-        // PrintFive()
-        //     .call(
-        //         String("Test"),
-        //         String("ing"),
-        //         String(" "),
-        //         String("Hmm"),
-        //         String("\n")
-        //         );
-
-        // Y().call(
-        //     Print(), String("Test")
-        //     );
-        Pipe().call(Println(), Square(), Number(200));
-        // Pipe().call(Print(), Square(), Number(100));
-
-        Print().call(String("square: "), Square().call(Number(500)), String("\n"));
-
-        Assert()
-            .call(
-                If().call(Identity().call(False()), False(), True())
-            );
-
-        Debug().call(
-            If().call(Identity().call(True()), String("Ok"), String("Not Ok"))
-        );
-
-        Debug().call(
-            If().call(
-                Equals().call(String("1"), String("2")),
-                    Number(1), Number(2.7646)
-                )
-        );
-
-        Debug().call(
-            Equals()
-                .call(Println(), Println())
-        );
-
-        Debug().call(
-            Add()
-                .call(Number(1), Number(2))
-        );
-
-        Debug().call(
-            Identity().call(Div()
-                .call(Number(13), Number(2)))
-        );
-
-        Debug().call(
-            False().call(
-                True().call(String("A"), String("B")),
-                True().call(String("C"), String("D"))
-            )
-        );
-
-        return Function();
-    }
+        template<typename __A__>
+        auto call(__A__ path) {
+                return Test().call(Square().call(Number(1.25)));
+        }
 };
 
-
 int main() {
-    Main().call();
+    Main().call(String("/"));
     return 0;
 }
