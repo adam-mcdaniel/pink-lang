6b6c31a2e5a75dc515ef1968c5f63d49041a242b
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2019-01-17 09:02:40 -0500

    Cleaned up collatz example

--- src/compiler/pf.py
+++ src/compiler/pf.py
@@ -55,30 +55,8 @@ except:
     sys.exit(1)
 
 
-
-# try:
-#     # process = Popen(
-#     #     args="g++ --std=c++14 target/main.cpp -o target/bin",
-#     #     shell=True
-#     # )
-#     process = subprocess.Popen(
-#         "g++ --std=c++14 target/main.cpp -o target/bin".split(),
-#         stdout=subprocess.PIPE,
-#         stderr=subprocess.PIPE
-#     )
-
-#     (out, err) = process.communicate()
-#     if err:
-#         print("Error:", str(err, "unicode"))
-#     # print("program error:", err)
-# except:
-#     error.error("Error during compilation. Did you typecheck your code?")
-# process = Popen(
-#     args="g++ --std=c++14 target/main.cpp -o target/bin",
-#     shell=True
-# )
 process = subprocess.Popen(
-    "g++ --std=c++14 target/main.cpp -o target/bin".split(),
+    "g++ --std=c++14 -O target/main.cpp -o target/bin".split(),
     stdout=subprocess.PIPE,
     stderr=subprocess.PIPE
 )
